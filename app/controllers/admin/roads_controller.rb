1c20b611f64794c6736f0f115cc5c935f13837b1
EC2 Default User <ec2-user@ip-172-31-39-196.ap-northeast-1.compute.internal>
2023-09-16 12:04:10 +0000

    Bootstrapの導入，マーカーのデザイン変更

--- app/controllers/admin/roads_controller.rb
+++ app/controllers/admin/roads_controller.rb
@@ -3,9 +3,11 @@ class Admin::RoadsController < ApplicationController
   end
 
   def create
-    file = params[:file]
+    file = params[:file] #フォームからファイルを受け取る
+    #CSV.foreach(ファイルのパス)で1行ずつ読み込む．オプション(encoding:文字コードの指定，headers:ファイル内にヘッダーがあるか)
     CSV.foreach(file.path, encoding: 'Shift_JIS:UTF-8', headers: true) do |row|
-      road = Road.find_or_create_by(number: row["路線"])
+      #各行の処理：ヘッダーがある場合row["ヘッダーにあるカラム名(日本語可)"]で列が指定できる
+      road = Road.find_or_create_by(number: row["路線"]) #find_or_create_by()で二重登録を防ぐ．
       latitude = row["緯度（度）"].to_f + row["緯度（分）"].to_f/60 + row["緯度（秒）"].to_f/3600
       longitude = row["経度（度）"].to_f + row["経度（分）"].to_f/60 + row["経度（秒）"].to_f/3600
       KiloPost.find_or_create_by(road_id: road.id, latitude: latitude, longitude: longitude)
@@ -14,7 +16,7 @@ class Admin::RoadsController < ApplicationController
   end
 
   def index
-    @roads = Road.all
+    @roads = Road.order(number: 'ASC')
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application";
+
 Rails.start()
 // Turbolinks.start()
 ActiveStorage.start()
